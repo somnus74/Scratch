f264b8d74f4119b303c58a9d6e4e056d1cbb19b6
Malcolm Edwards <nazxuul@gmail.com>
2018-09-10 09:25:46 +1000

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Malcolm Edwards on 10/9/18.
+//  Copyright © 2018 Xenophile Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
