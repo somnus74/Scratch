54df0efcaeaffa1ac9a0e4e21047950c16e91c9f
Malcolm Edwards <nazxuul@gmail.com>
2018-09-18 10:30:53 +1000

    lecture-79

--- Smack/Services/UserDataService.swift
+++ Smack/Services/UserDataService.swift
@@ -57,5 +57,17 @@ class UserDataService {
 
         return UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
     }
+    
+    func logoutUser() {
+        id = ""
+        avatarName = ""
+        avatarColor = ""
+        email = ""
+        name = ""
+        AuthService.instance.isLoggedIn = false
+        AuthService.instance.userEmail = ""
+        AuthService.instance.authToken = ""
+    }
+    
 }
 
